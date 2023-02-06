e0826d9a76302ee7e2823b958eea08c43e73bfbe
sogood17 <100080482+sogood17@users.noreply.github.com>
2023-02-06 22:50:15 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+const openButton = document.querySelector(".open");
+const container = document.querySelector(".container");
+const closeButton = document.querySelector(".close");
+
+openButton.addEventListener("click", () => {
+  container.style.display = "flex";
+  openButton.style.display = "none";
+});
+
+closeButton.addEventListener("click", () => {
+  container.style.display = "none";
+  openButton.style.display = "block";
+});
